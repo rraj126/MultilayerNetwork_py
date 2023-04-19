3d28ab169f378ef4a62903d0c653afc7d6cba0b8
rraj <rishabh.bsbe@gmail.com>
2023-04-19 01:17:18 -0500

    get to data organized with classes

--- preprocess.py
+++ preprocess.py
@@ -3,20 +3,43 @@ import numpy as np
 from typing import List
 import warnings
 
+def clean_data(data_array):
+    #function to clean data
+    #need additional cleaning steps like normalization
+    data_array[np.isnan(data_array)] = 0 # type: ignore
+
+    return data_array
+
+def get_sheet_data(f:dict, sheet:str):
+    df = f[sheet]
+
+    data_as_array = df.to_numpy()
+    processed_data = clean_data(data_as_array)
+    data_size = processed_data.shape
+    
+    return processed_data, data_size
+
+def get_sheet_labels(f:dict, sheet:str, ncols:int, header:bool):
+    df = f[sheet]
+    
+    if header:
+        data_labels = list(df.columns.values)
+        if len(data_labels) != ncols:
+            raise Exception("improper labels")
+    else:
+        data_labels = ['<ukn>']*ncols
+
+    return data_labels
+        
+
 def read_sheet(f:dict, sheet:str, header:bool = True):
     sheet_dict = {}
     
-    df = f[sheet]
-    data_as_array = df.to_numpy()
-    data_as_array[np.isnan(data_as_array)] = 0
-    
-    sheet_dict['values'] = data_as_array
-    
-    if header:
-        sheet_dict['labels'] = list(df.columns.values)
-    else:
-        sheet_dict['labels'] = None
-        
+    data_values, data_size = get_sheet_data(f, sheet)
+    sheet_dict['values'] = data_values
+    sheet_dict['size'] = data_size
+    sheet_dict['labels'] = get_sheet_labels(f, sheet, int(data_size[1]), header)
+
     return sheet_dict
 
 def read_data_file(file_path:str, header:bool = True, sheets:List[str] = []):
@@ -28,8 +51,7 @@ def read_data_file(file_path:str, header:bool = True, sheets:List[str] = []):
     if sheets:
         for sheet in sheets:
             if sheet in sheet_names_from_file:
-                data_dict[sheet] = read_sheet(f, sheet, header=header)
-                
+                data_dict[sheet] = read_sheet(f, sheet, header=header)          
             else:
                 warnings.warn(f"sheet {sheet} not in file")
     else:
@@ -37,3 +59,36 @@ def read_data_file(file_path:str, header:bool = True, sheets:List[str] = []):
             data_dict[sheet] = read_sheet(f, sheet)
         
     return data_dict
+
+
+def get_unique_data_labels(data_dict:dict):
+    list_of_lists = [data_dict[sheet]['labels'] for sheet in data_dict.keys()]
+    list_of_labels = []
+    for each_list in list_of_lists:
+        for each_label in each_list:
+            list_of_labels.append(each_label)
+
+    unique_labels = set(list_of_labels)
+    nclass = len(unique_labels)
+
+    return unique_labels, nclass
+   
+
+def organize_data_with_labels(data_dict:dict):
+    unique_labels, nclass = get_unique_data_labels(data_dict)
+    data_dims = [data_dict[sheet]['size'][0] for sheet in data_dict.keys()]
+    
+    if len(set(data_dims)) != 1:
+        raise Exception("inconsistent data dimensions")
+    else:
+        organized_data = {}
+        
+        for label in unique_labels:
+            labeled_data = np.array([], dtype = np.float64).reshape(data_dims[0], 0)
+            for sheet in data_dict.keys():
+                data_index = [l == label for l in data_dict[sheet]['labels']]
+                np.hstack([labeled_data, data_dict[sheet]['values'][:, data_index]])
+            
+            organized_data[label] = labeled_data
+
+    return organized_data
